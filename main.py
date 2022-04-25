e6eb21fa7afd5d05dff67c50b3e8c07a4cc07c32
dustin-heider <58644408+dustin-heider@users.noreply.github.com>
2022-04-25 14:05:57 +0200

    New Design with red color, raised some Exceptions if input is not an Integer or not in range 0 - 16

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from random import shuffle, sample
-from pandas import *
 from tkinter import *
 from datetime import datetime
-import pyperclip
 
 FONT_NORMAL = ("Arial", 11, "normal")
 FONT_BOLD = ("Arial", 11, "bold")
@@ -14,7 +12,6 @@ def row_down():
     y_position += 176
 
 
-# data = read_csv("data/Bingo_Kategorien.CSV", encoding="ISO-8859-1").dropna()
 easy_categories = ['"Historisch"', '"INRI"', '"Hier sieht\nman ..."', '"Bitte Standort\nx nehmen"',
                    '"Jung & Alt /\nGroß & Klein"', '"Kein\nPrivatgrundstück"', 'PokéStop-Variation', '3x gleicher Text',
                    'Beschreibung mit\neinem Wort', 'Verweis auf\ndie Kriterien', 'Anführungszeichen\nim Text',
@@ -35,42 +32,47 @@ hard_categories = ['"Mural"', '"Anno / Erbaut"', '"Graffiti" falsch\ngeschrieben
                    'Wegweiser\nzum Objekt', 'Museum', 'Litfaßsäule', 'Maibaum', 'Casino / Stripclub', 'Grenzstein',
                    'Kaugummi-Automat', 'Objekt im Kreisel', 'Wayspot im Wasser', 'Saisonale Deko']
 
-
 name = input("Name: ")
-number_hard_categories = int(input("Mit wie vielen schwierigen Kategorien möchtest du spielen? (Zahl von 0 - 16) "))
-if number_hard_categories < 0 or number_hard_categories > 16:
-    number_hard_categories = 0
+if name == "":
+    name = "Standard-Bingo"
+try:
+    number_hard_categories = int(input("Mit wie vielen schwierigen Kategorien möchtest du spielen? (Zahl von 0 - 16) "))
+except ValueError:
+    print(f"{ValueError}. Die Eingabe enthält keine Zahl!")
+else:
+    if number_hard_categories < 0 or number_hard_categories > 16:
+        raise ValueError("Nummer muss im Bereich 0 - 16 liegen!")
 
-number_easy_categories = 16 - number_hard_categories
-easy_category_list = sample(easy_categories, number_easy_categories)
-hard_category_list = sample(hard_categories, number_hard_categories)
-category_list = easy_category_list + hard_category_list
-shuffle(category_list)
-# category_string = "\n".join(category_list)
-# pyperclip.copy(category_string)
+    number_easy_categories = 16 - number_hard_categories
+    easy_category_list = sample(easy_categories, number_easy_categories)
+    hard_category_list = sample(hard_categories, number_hard_categories)
+    category_list = easy_category_list + hard_category_list
+    shuffle(category_list)
+    # category_string = "\n".join(category_list)
+    # pyperclip.copy(category_string)
 
-window = Tk()
-window.title("Wayfarer D/A/CH Bingo")
-canvas = Canvas(height=881, width=836, background="white")
-template = PhotoImage(file="./data/Bingo 4x4_Feld.png")
-canvas.create_image(418, 440.5, image=template)
-canvas.pack()
+    window = Tk()
+    window.title("Wayfarer D/A/CH Bingo")
+    canvas = Canvas(height=881, width=836, background="white")
+    template = PhotoImage(file="./data/Bingo 4x4_Feld.png")
+    canvas.create_image(418, 440.5, image=template)
+    canvas.pack()
 
-x_position = 135
-y_position = 233
-count = 0
-datum = datetime.today().strftime('%d.%m.%y')
-for item in category_list:
-    canvas.create_text(x_position, y_position, justify="center", text=item, font=FONT_NORMAL)
-    x_position += 187
-    count += 1
-    if count % 4 == 0:
-        row_down()
+    x_position = 135
+    y_position = 233
+    count = 0
+    datum = datetime.today().strftime('%d.%m.%y')
+    for item in category_list:
+        canvas.create_text(x_position, y_position, justify="center", text=item, font=FONT_NORMAL)
+        x_position += 187
+        count += 1
+        if count % 4 == 0:
+            row_down()
 
-canvas.create_text(697, 78, text="Regeln:\n"
-                                  "1 Punkt pro Feld\n"
-                                  "3 Punkte pro Reihe (Bingo)", justify="center", font=FONT_BOLD)
-canvas.create_text(135, 68, text=f"Datum: {datum}", justify="center", font=FONT_BOLD)
-canvas.create_text(135, 88, text=f"Name: {name}", justify="center", font=FONT_BOLD)
+    canvas.create_text(697, 78, text="Regeln:\n"
+                                      "1 Punkt pro Feld\n"
+                                      "3 Punkte pro Reihe (Bingo)", justify="center", font=FONT_BOLD)
+    canvas.create_text(133, 68, text=f"Datum: {datum}", justify="center", font=FONT_BOLD)
+    canvas.create_text(133, 88, text=f"Name: {name}", justify="center", font=FONT_BOLD)
 
-window.mainloop()
+    window.mainloop()
