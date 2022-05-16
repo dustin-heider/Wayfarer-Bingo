53c099d563ea375a863de8faeeb774e3d82af8e4
dustin-heider <58644408+dustin-heider@users.noreply.github.com>
2022-05-16 13:57:02 +0200

    Changed some categories

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from datetime import datetime
 FONT_NORMAL = ("Arial", 13, "normal")
 FONT_BOLD = ("Arial", 11, "bold")
 STANDARD_DIFFICULTY = 4
-DATUM = datetime.today().strftime('%d.%m.%y')
 
 
 def row_down():
@@ -15,32 +14,44 @@ def row_down():
 
 
 easy_categories = ['"Historisch"', '"INRI"', '"Hier sieht\nman ..."', '"Bitte Standort\nx nehmen"',
-                   '"Jung & Alt /\nGroß & Klein"', '"Kein\nPrivatgrundstück"', 'PokéStop-Variation', '3x gleicher Text',
-                   'Beschreibung mit\neinem Wort', 'Wort mit\n12+ Buchstaben', '"Danke für die\n Bewertung" / "Danke"',
-                   'Verweis auf\ndie Kriterien', 'Anführungszeichen\nim Text',
-                   '"Unnötiger" Edit', 'Rechtschreibungs-\nEdit', 'Leere\nDuplikatsleiste', 'Duplikat',
+                   '"Jung & Alt /\nGroß & Klein"', '"Kein\nPrivatgrundstück"', '"Danke für die\n Bewertung" / "Danke"',
+                   '"Lädt zum\nVerweilen ein"',
+
+                   'PokéStop-Variation', '3x gleicher Text', 'Beschreibung mit\neinem Wort', 'Wort mit\n12+ Buchstaben',
+                   'Verweis auf\ndie Kriterien', 'Anführungszeichen\nim Text', 'Wayfarer-Referenz',
+                   'Referenz zu einem\nNiantic-Spiel',
+
+                   '"Unnötiger" Edit', 'Rechtschreibungs-\nEdit',
+
+                   'Leere\nDuplikatsleiste', 'Duplikat',
+
                    'Haupt- &\nZusatzbild gleich', 'Schatten auf\ndem Wayspot', 'Schlechte Ausrichtung', 'Rangezoomt',
-                   'Nummernschild\nerkennbar', 'Stromkasten', 'Aussichtspunkt', 'Sportplatz', 'Vereinsheim',
-                   'Spielplatz', 'Tischtennisplatte', 'Feuerwehr/\n(-Gedenkstein)', 'Baumarktlöwe', 'Stolperstein',
-                   'Generisches\nStraßenschild', 'Sitzbank', 'Wayfarer-Referenz', 'Wasserpumpe/Brunnen',
+                   'Nummernschild\nerkennbar', 'Wayspot auf Schul-/\nKindergartengelände',
+
+                   'Stromkasten', 'Aussichtspunkt', 'Sportplatz', 'Vereinsheim', 'Spielplatz', 'Tischtennisplatte',
+                   'Feuerwehr/\n(-Gedenkstein)', 'Baumarktlöwe', 'Stolperstein',
+                   'Generisches\nStraßenschild', 'Sitzbank', 'Brunnen/\nWasserpumpe',
                    'Gefallenendenkmal', 'Skulptur / Statue', 'Kleingartenverein', 'Insektenhotel',
-                   'Naturmerkmal', 'Wanderwegweiser', 'Kirche', 'Fahrrad im Bild',
+                   'Naturmerkmal', 'Wanderwegweiser', 'Kirche', 'Schule/Kindergarten', 'Fahrrad im Bild',
                    'Wanderkarte/\nOrtskarte', 'Name des Spielgeräts\nim Titel']
 
-hard_categories = ['"Mural" falsch\nbenutzt', '"Anno / Erbaut"', '"Graffiti" falsch\ngeschrieben',
-                   'Alliteration\n(3+ Wörter)', 'Wort in ALL CAPS', 'Ingress-Referenz', 'Link in Zusatzinfo',
-                   'Beleidigung in\nZusatzinfo', 'Denglisches Wort', 'Platzhalter-Texte', 'Emoji / Emoticon',
-                   'Foto-Edits\n(3+ Bilder)', 'Standort-Edits\n(4+ Standorte)', 'Foto mit\nWasserzeichen',
-                   'Bildschirmfoto/\nScreenshot', 'Geotag im Zusatzbild', 'Aktuelles Streetview\n(bis 1 Jahr)',
-                   'Finger vor\nder Linse', 'Foto aus Auto/\nGebäude', 'Fuß/Schuh\nim Bild', 'Lebendiges Tier\nim Bild',
-                   'Gedenkplakette bei\neinem Straßenschild', 'Baum verdeckt\nWayspot', 'Sonnenuhr', 'Gullydeckel',
-                   'Wegweiser\nzum Objekt', 'Museum', 'Litfaßsäule', 'Maibaum',
-                   'Grenzstein/OD-Stein/\nKilometerstein', 'Ort mit 10+\nBuchstaben',
-                   'Kaugummi-Automat', 'Objekt im Kreisel', 'Wayspot im Wasser', 'Schützenkönig/in-\nScheibe',
-                   'Post-/Briefkasten', 'Spiegelung des\nFotografen', 'Spielplatz mit\n4+ Wayspots', 'Fitness-Station',
-                   'Saisonale Deko']
+hard_categories = ['"Mural" falsch\nbenutzt', '"Anno / Erbaut"', '"Graffito/Graffiti"\nfalsch geschrieben',
+                   'Alliteration\n(3+ Wörter)', 'Wort in ALL CAPS', 'Link in Zusatzinfo', 'Beleidigung in\nZusatzinfo',
+                   'Denglisches Wort', 'Platzhalter-Texte', 'Emoji / Emoticon', 'Texte in verschiedenen\nSprachen',
 
-# hard_categories_redacted = ['Casino / Stripclub',]
+                   'Foto-Edits\n(3+ Bilder)', 'Standort-Edits\n(4+ Standorte)',
+
+                   'Foto mit\nWasserzeichen', 'Bildschirmfoto/\nScreenshot', 'Geotag im Zusatzbild',
+                   'Aktuelles Streetview\n(bis 1 Jahr)', 'Finger vor\nder Linse', 'Foto aus Auto/\nGebäude',
+                   'Fuß/Schuh\nim Bild', 'Lebendiges\nTier im Bild',
+
+                   'Gedenkplakette bei\neinem Straßenschild', 'Baum verdeckt\nWayspot', 'Sonnenuhr', 'Gullydeckel',
+                   'Wegweiser\nzum Objekt', 'Museum', 'Litfaßsäule', 'Maibaum', 'Grenzstein/OD-Stein/\nKilometerstein',
+                   'Ort mit 10+\nBuchstaben', 'Kaugummi-/\nZigarettenautomat', 'Objekt im Kreisel', 'Wayspot im Wasser',
+                   'Schützenkönig/in-\nScheibe', 'Post-/Briefkasten', 'Spiegelung des\nFotografen',
+                   'Spielplatz mit\n4+ Wayspots', 'Fitness-Station', 'Saisonale Deko']
+
+# hard_categories_redacted = ['Collage', 'Casino / Stripclub',]
 
 players_remaining = True
 while players_remaining:
@@ -75,6 +86,7 @@ while players_remaining:
         x_position = 135
         y_position = 233
         count = 0
+        datum = datetime.today().strftime('%d.%m.%y')
         for item in category_list:
             canvas.create_text(x_position, y_position, justify="center", text=item, font=FONT_NORMAL)
             x_position += 187
@@ -85,7 +97,7 @@ while players_remaining:
         canvas.create_text(135, 94, text="Regeln:\n"
                                          "1 Punkt pro Feld\n"
                                          "3 Punkte pro Reihe (Bingo)", justify="center", font=FONT_BOLD)
-        canvas.create_text(697, 94, text=f"Datum: {DATUM}\n"
+        canvas.create_text(697, 94, text=f"Datum: {datum}\n"
                                          f"Name: {name}\n"
                                          f"Schwierige Kategorien: {number_hard_categories}", justify="center",
                                          font=FONT_BOLD)
