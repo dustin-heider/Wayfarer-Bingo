53904a30da1ca9abb30b6359be13188224ce98bf
dustin-heider <58644408+dustin-heider@users.noreply.github.com>
2022-08-02 13:15:52 +0200

    added comments

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from datetime import datetime
 
 FONT_NORMAL = ("Arial", 13, "normal")
 FONT_BOLD = ("Arial", 11, "bold")
-STANDARD_DIFFICULTY = 4
+STANDARD_DIFFICULTY = 5         # change if necessary
 
 
+# one row down if 4 categories are in one row
 def row_down():
     global x_position, y_position
     x_position = 135
     y_position += 176
 
-
+# add/delete categories in these two lists
 easy_categories = ['"Historisch"', '"INRI"', '"Hier sieht\nman ..."', '"Bitte Standort\nx nehmen"',
                    '"Jung & Alt /\nGroß & Klein"', '"Kein\nPrivatgrundstück"', '"Danke für die\n Bewertung" / "Danke"',
                    '"Lädt zum\nVerweilen ein"',
@@ -26,14 +27,14 @@ easy_categories = ['"Historisch"', '"INRI"', '"Hier sieht\nman ..."', '"Bitte St
                    'Leere\nDuplikatsleiste', 'Duplikat',
 
                    'Haupt- &\nZusatzbild gleich', 'Schatten auf\ndem Wayspot', 'Schlechte Ausrichtung', 'Rangezoomt',
-                   'Nummernschild\nerkennbar', 'Wayspot auf Schul-/\nKindergartengelände',
+                   'Nummernschild\nerkennbar', 'Wayspot auf Schul-/\nKindergartengelände', 'Nutzloses\nUmgebungsfoto',
 
                    'Stromkasten', 'Aussichtspunkt', 'Sportplatz', 'Vereinsheim', 'Spielplatz', 'Tischtennisplatte',
                    'Feuerwehr/\n(-Gedenkstein)', 'Baumarktlöwe', 'Stolperstein',
                    'Generisches\nStraßenschild', 'Sitzbank', 'Brunnen/\nWasserpumpe',
                    'Gefallenendenkmal', 'Skulptur / Statue', 'Kleingartenverein', 'Insektenhotel',
                    'Naturmerkmal', 'Wanderwegweiser', 'Kirche', 'Schule/Kindergarten', 'Fahrrad im Bild',
-                   'Wanderkarte/\nOrtskarte', 'Name des Spielgeräts\nim Titel']
+                   'Wanderkarte/\nOrtskarte', 'Name des Spielgeräts\nim Titel', 'Willkommensschild']
 
 hard_categories = ['"Mural" falsch\nbenutzt', '"Anno / Erbaut"', '"Graffito/Graffiti"\nfalsch geschrieben',
                    'Alliteration\n(3+ Wörter)', 'Wort in ALL CAPS', 'Link in Zusatzinfo', 'Beleidigung in\nZusatzinfo',
@@ -53,6 +54,8 @@ hard_categories = ['"Mural" falsch\nbenutzt', '"Anno / Erbaut"', '"Graffito/Graf
 
 # hard_categories_redacted = ['Collage', 'Casino / Stripclub',]
 
+# function to get and process inputs, creates Tkinter window with categories, screenshot window to save file
+# possibility of Ghostscript to save a file without screenshot?
 players_remaining = True
 while players_remaining:
     name = input("Name: ")
@@ -60,46 +63,47 @@ while players_remaining:
         break
     if name == "":
         name = "Standard-Bingo"
-    try:
-        number_hard_categories = int(input("Mit wie vielen schwierigen Kategorien möchtest du spielen?"
-                                           "(Zahl von 0 - 16) "))
-    except ValueError:
-        print(f"{ValueError}. Die Eingabe enthält keine Zahl!")
-    else:
-        if number_hard_categories < 0 or number_hard_categories > 16:
-            number_hard_categories = STANDARD_DIFFICULTY
-            print(f"Standard-Schwierigkeitsgrad ({number_hard_categories} schwierige Kategorien) wurde ausgewählt!")
 
-        number_easy_categories = 16 - number_hard_categories
-        easy_category_list = sample(easy_categories, number_easy_categories)
-        hard_category_list = sample(hard_categories, number_hard_categories)
-        category_list = easy_category_list + hard_category_list
-        shuffle(category_list)
+    number_hard_categories = input("Mit wie vielen schwierigen Kategorien möchtest du spielen?"
+                                    "(Zahl von 0 - 16): ")
+    if len(number_hard_categories) == 0:
+        number_hard_categories = STANDARD_DIFFICULTY
+    elif not number_hard_categories.isdigit():
+        break
 
-        window = Tk()
-        window.title("Wayfarer D/A/CH Bingo")
-        canvas = Canvas(height=881, width=836, background="white")
-        template = PhotoImage(file="./data/Bingo 4x4_new.png")
-        canvas.create_image(418, 440.5, image=template)
-        canvas.pack()
+    number_hard_categories = int(number_hard_categories)
+    if number_hard_categories < 0 or number_hard_categories > 16:
+        break
+    number_easy_categories = 16 - number_hard_categories
+    easy_category_list = sample(easy_categories, number_easy_categories)
+    hard_category_list = sample(hard_categories, number_hard_categories)
+    category_list = easy_category_list + hard_category_list
+    shuffle(category_list)
 
-        x_position = 135
-        y_position = 233
-        count = 0
-        datum = datetime.today().strftime('%d.%m.%y')
-        for item in category_list:
-            canvas.create_text(x_position, y_position, justify="center", text=item, font=FONT_NORMAL)
-            x_position += 187
-            count += 1
-            if count % 4 == 0:
-                row_down()
+    window = Tk()
+    window.title("Wayfarer D/A/CH Bingo")
+    canvas = Canvas(height=881, width=836, background="white")
+    template = PhotoImage(file="./data/Bingo 4x4_new.png")
+    canvas.create_image(418, 440.5, image=template)
+    canvas.pack()
 
-        canvas.create_text(135, 94, text="Regeln:\n"
-                                         "1 Punkt pro Feld\n"
-                                         "3 Punkte pro Reihe (Bingo)", justify="center", font=FONT_BOLD)
-        canvas.create_text(697, 94, text=f"Datum: {datum}\n"
-                                         f"Name: {name}\n"
-                                         f"Schwierige Kategorien: {number_hard_categories}", justify="center",
-                                         font=FONT_BOLD)
+    x_position = 135
+    y_position = 233
+    count = 0
+    datum = datetime.today().strftime('%d.%m.%y')
+    for item in category_list:
+        canvas.create_text(x_position, y_position, justify="center", text=item, font=FONT_NORMAL)
+        x_position += 187
+        count += 1
+        if count % 4 == 0:
+            row_down()
 
-        window.mainloop()
+    canvas.create_text(135, 94, text="Regeln:\n"
+                                     "1 Punkt pro Feld\n"
+                                     "3 Punkte pro Reihe (Bingo)", justify="center", font=FONT_BOLD)
+    canvas.create_text(697, 94, text=f"Datum: {datum}\n"
+                                     f"Name: {name}\n"
+                                     f"Schwierige Kategorien: {number_hard_categories}", justify="center",
+                                     font=FONT_BOLD)
+
+    window.mainloop()
